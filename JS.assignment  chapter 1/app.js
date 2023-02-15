c45ae3662e93fd1b0cd1c9bcd6f06c2cdace2c82
Zeeshan Ahmed <122614629+ZeeshanAhmead@users.noreply.github.com>
2023-02-15 19:59:15 +0500

    JS.assignment chapter 1

---
+++ JS.assignment  chapter 1/app.js
@@ -0,0 +1,105 @@
+// chapter no 1  (Alert)
+
+// Q 2
+// alert ("Error! Please enter a valid password");
+
+// Q 3
+// alert ("Welcome to JS Land \n Happy Coding");
+
+// Q 4
+// alert ("Welcome to JS Land");
+// alert ("Happy Coding");
+
+// Q 5
+// console.log("Hallo... I can run JS through my web browser`s console");
+
+// let sub1 = parseInt(window.prompt("Enter marks for subject 1:"));
+// let sub2 = parseInt(window.prompt("Enter marks for subject 2:"));
+
+// let totalMarks = sub1 + sub2;
+// let percentage = (totalMarks / 200) * 100;
+
+// document.write("<table border='1'>");
+// document.write("<tr>");
+// document.write("<th>Subject 1</th>");
+// document.write("<th>Subject 2</th>");
+// document.write("<th>Total Marks</th>");
+// document.write("<th>Percentage</th>");
+// document.write("</tr>");
+
+// document.write("<tr>");
+// document.write("<td>" + sub1 + "</td>");
+// document.write("<td>" + sub2 + "</td>");
+// document.write("<td>" + totalMarks + "</td>");
+// document.write("<td>" + percentage + "%</td>");
+// document.write("</tr>");
+
+// document.write("</table>");
+
+// let num = parseInt(window.prompt("Enter a number:"));
+
+// if (num % 3 === 0) {
+//   document.write(`The number ${num} is divisible by 3.`);
+// } else {
+//   document.write(`The number ${num} is not divisible by 3.`);
+// }
+
+// let color = window.prompt("Enter color of road traffic signal:");
+
+// switch (color) {
+//   case "Red":
+//     document.write("Stop");
+//     break;
+//   case "Yellow":
+//     document.write("Ready to move");
+//     break;
+//   case "Green":
+//     document.write("Move now");
+//     break;
+//   default:
+//     document.write("Invalid color");
+// }
+
+// let sub1 = parseInt(window.prompt("Enter marks for subject 1:"));
+// let sub2 = parseInt(window.prompt("Enter marks for subject 2:"));
+// let sub3 = parseInt(window.prompt("Enter marks for subject 3:"));
+// let totalMarks = parseInt(window.prompt("Enter total marks:"));
+
+// let obtainedMarks = sub1 + sub2 + sub3;
+// let percentage = (obtainedMarks / totalMarks) * 100;
+
+// document.write("<table border='1'>");
+// document.write("<tr>");
+// document.write("<th>Subject 1</th>");
+// document.write("<th>Subject 2</th>");
+// document.write("<th>Subject 3</th>");
+// document.write("<th>Total Marks</th>");
+// document.write("<th>Obtained Marks</th>");
+// document.write("<th>Percentage</th>");
+// document.write("<th>Grade</th>");
+// document.write("</tr>");
+
+// document.write("<tr>");
+// document.write("<td>" + sub1 + "</td>");
+// document.write("<td>" + sub2 + "</td>");
+// document.write("<td>" + sub3 + "</td>");
+// document.write("<td>" + totalMarks + "</td>");
+// document.write("<td>" + obtainedMarks + "</td>");
+// document.write("<td>" + percentage + "%</td>");
+
+// if (percentage >= 80) {
+//   document.write("<td>A-one</td>");
+// } else if (percentage >= 70) {
+//   document.write("<td>A</td>");
+// } else if (percentage >= 60) {
+//   document.write("<td>B</td>");
+// } else if (percentage >= 50) {
+//   document.write("<td>C</td>");
+// } else if (percentage >= 40) {
+//   document.write("<td>D</td>");
+// } else {
+//   document.write("<td>Fail</td>");
+// }
+
+// document.write("</tr>");
+// document.write("</table>");
