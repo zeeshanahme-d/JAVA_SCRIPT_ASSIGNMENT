8357d1de67921d45796191017f8153bb4d8889bc
Zeeshan Ahmed <122614629+ZeeshanAhmead@users.noreply.github.com>
2023-02-15 20:00:47 +0500

    JS Assignment Ch# 9 to 11

---
+++ JS Assignment Ch# 9 to 11/app.js
@@ -0,0 +1,136 @@
+// Chapter 9 to 11
+
+// Q 1
+
+// var city = prompt("Enter your City Name");
+
+// if(city === 'Karachi'){
+//     alert('Welcome to city of lights');
+// }
+// else{
+//     alert('Welcome');
+// }
+
+// Q 2
+
+// var gender = prompt("Enter your Gender");
+
+// if (gender === 'Male', 'male') {
+//     alert('Good Morning Sir');
+// }
+// else if (gender === 'Female', 'female') {
+//     alert("Good Morning Ma'am");
+// }
+
+// Q 3
+
+// var signalColor = prompt("Enter Color of Traffic Signal","xyz,xyz,xyz");
+
+// document.write('<table> <tr> <th>Signal color</th> <th></th> <th></th> <th>Message</th> </tr>');
+// document.write(' <tr> <td>Red</td> <td></td> <td></td> <td>Must stop</td> </tr>');
+// document.write(' <tr> <td>Yellow</td> <td></td> <td></td> <td>Ready to move</td> </tr>');
+// document.write(' <tr> <td>Green</td> <td></td> <td></td> <td>Move now</td> </tr> </table');
+
+// Q 4
+
+// var remFul = prompt("How much fuel in your car", "1leters");
+
+// if (remFul === "0.25leters") {
+//     alert("Please refill the fuel in your car");
+// }
+// else {
+// alert("Have a nice day:");
+// }
+
+// Q 5
+
+// Q 6
+
+// var name = prompt("Enter your Name")
+// var totalMarks = prompt("Enter Your Total Marks:")
+// var obtainedMarks = prompt("Enter Your Obtained Marks:")
+// var percentage = obtainedMarks / totalMarks * 100 + "%"
+// if (percentage >= "80") {
+
+//    document.write("<b> Name: " + name + "</b> <br /> Total marks: " + totalMarks + "<br /> Marks obtained: " + obtainedMarks + "<br /> percentage: " + percentage + "<br /> Grade: A-one <br /> Remark: Excellent")
+
+// }
+// else if (percentage >= "70") {
+
+//    document.write("<b> Name: " + name + "</b> <br /> Total marks: " + totalMarks + "<br /> Marks obtained: " + obtainedMarks + "<br /> percentage: " + percentage + "<br /> Grade: A <br /> Remark: Good")
+
+// }
+// else if (percentage >= "60") {
+
+//    document.write("<b> Name: " + name + "</b> <br /> Total marks: " + totalMarks + "<br /> Marks obtained: " + obtainedMarks + "<br /> percentage: " + percentage + "<br /> Grade: B <br /> Remark: You need to Improve")
+
+// }
+// else if (percentage < "60") {
+
+//    document.write("<b> Name: " + name + "</b> <br /> Total marks: " + totalMarks + "<br /> Marks obtained: " + obtainedMarks + "<br /> percentage: " + percentage + "<br /> Grade: Fail <br /> Remark: Try next time")
+
+// }
+
+// Q 7
+
+// var a = prompt("Guess The Secret Number")
+// if (a === "7") {
+//    alert("Bingo! Correct Answer:")
+// }
+// else if (a === "6" || "8") {
+//    alert("Close enough to the correct Answer:")
+// }
+// // else if (a === "8"){
+// //    alert("Close enough to the correct Answer:")
+// // }
+// else{
+//    alert("Wrong Answer:")
+// }
+
+// Q 8
+
+// Q 9
+
+// var number = prompt("Check the number is even or odd:")
+// var reminder = (number % 2)
+// if (reminder == "0") {
+//    alert( number + " is Even:")
+// }
+// else {
+//    alert( number + " is Odd:")
+// }
+
+// Q 10
+
+// var Temperature = prompt("Check Temperature")
+// if (Temperature >= 31 && Temperature <= 50) {
+//     alert("It is too hot outside:")
+// }
+// else if (Temperature >= 21 && Temperature <= 30) {
+//     alert("The Weather today is Normal.")
+// }
+// else if (Temperature >= 11 && Temperature <= 20) {
+//     alert("Today Weather is cool.")
+// }
+// else (Temperature <= 10)
+// alert("OMG! Today weather is so Cool.")
+
+
+// Q 11
+
+// var a = + prompt("Enter First Number")
+// var b = prompt("Enter operaters")
+// var c = + prompt("Enter second Number")
+
+// if (b === "+") {
+//     alert(a + c)
+// }
+// else if (b === "-") {
+//     alert(a - c)
+// }
+// else if (b === "/") {
+//     alert(a / c)
+// }
+// else if (b === "*") {
+//     alert(a * c)
+// }
