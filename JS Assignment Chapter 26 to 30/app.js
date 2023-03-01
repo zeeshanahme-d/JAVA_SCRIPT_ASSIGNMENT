2d8df7cf5661dd30faaec1b1d7ce933eb92f8d30
Zeeshan Ahmed <122614629+ZeeshanAhmead@users.noreply.github.com>
2023-03-02 00:50:20 +0500

    JS Assignment Chapter 26 to 30

---
+++ JS Assignment Chapter 26 to 30/app.js
@@ -0,0 +1,62 @@
+// Chapter No 26 to 30
+
+// Q 1
+
+// var userInput = prompt("Enter a positive Integer:");
+// document.write("Number: " + userInput + "<br>")
+// document.write("Round of value: " + Math.round(userInput) + "<br>");
+// document.write("Floor value is : " + Math.floor(userInput) + "<br>");
+// document.write("ceil value is : " + Math.ceil(userInput) + "<br>");
+
+// Q 2
+
+// var userInput = prompt("Enter a Negative Integer:");
+// document.write("Number: " + userInput + "<br>")
+// document.write("Round of value: " + Math.round(userInput)+ "<br>");
+// document.write("Floor value is : " + Math.floor(userInput)+ "<br>");
+// document.write("ceil value is : " + Math.ceil(userInput)+ "<br>");
+
+// Q 3
+
+// var num1 = prompt("Enter a Number Chacke Absolut Value:");
+// var num = Math.abs(num1)
+// document.write("The Absolut Value of " + num1 + " is " + num);
+
+// Q 4
+
+
+// var num = Math.random() * 6 + 1;
+// var num3 = Math.floor(num);
+// document.write("Random dice value: "+num3);
+
+// Q 5
+
+// var num = Math.random() * 2 + 1;
+// var result = Math.floor(num)
+// document.write("Random Coin value: Heads " + result + "<br>")
+
+// var num = Math.random() * 2;
+// var result = Math.floor(num)
+// document.write("Random Coin value: Heads " + result + "<br>")
+
+// Q 6
+
+// var num = Math.random() * 100;
+// var num1 = Math.floor(num)
+// document.write("Random Number between 1 to 100 is: " + num1)
+
+// Q 7///////////////
+
+
+// Q 8
+
+// var num = Math.random() * 10 + 1;
+// var num2 = Math.floor(num);
+// var userInput = parseInt(prompt("Guess the secret number (between 1 and 10):"));
+// if (userInput === num2) {
+//     document.write("Congratulations! You guessed the secret number " + userInput + "!");
+// }
+// else {
+//     document.write("Sorry, the secret number was " +num2 + ". Try again!");
+
+// }
